4c118ce49affcd9de8ac8fa3b781913f552d3128
vanshika622 <73771352+vanshika622@users.noreply.github.com>
2022-10-13 19:42:13 +0530

    Create binarysearch.cpp

---
+++ binarysearch.cpp
@@ -0,0 +1,17 @@
+int binarysearch(int arr[], int n, int k) {
+       int low=0;
+       int high=n-1;
+       while(low<=high){
+           int mid= low+ (high-low)/2;
+           if(arr[mid]==k){
+               return mid;
+           }
+           else if(arr[mid]>k){
+               high=mid-1;
+           }
+           else{
+               low=mid+1;
+           }
+       }
+       return -1;
+    }
