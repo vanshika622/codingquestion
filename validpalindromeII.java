264e200384a25b3ba2b74db117a3811535b09ce0
vanshika622 <73771352+vanshika622@users.noreply.github.com>
2022-10-25 17:36:40 +0530

    Create validpalindromeII.java
    validpalindromeII

---
+++ validpalindromeII.java
@@ -0,0 +1,26 @@
+public class Solution {
+ public static boolean check(String s, int i, int j){
+     while(i<=j){
+         if(s.charAt(i)!=s.charAt(j))
+             return false;
+         
+         i++;
+         j--;
+     }
+     return true;
+     
+ }
+	public static boolean validPalindrome(int n, String s) {
+		int i=0;
+        int j = s.length()-1;
+         while(i<j){
+             if(s.charAt(i)!=s.charAt(j)){
+                 return check(s,i,j-1)|| check(s,i+1,j);
+             }
+             i++;
+             j--;
+         }
+        return true;
+	}
+
+}
